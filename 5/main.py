dd816cd52614bb7d1e09bc53716d0a797575f01d
Pratik Jadhav <81086797+Pratik-geek@users.noreply.github.com>
2023-05-21 17:54:32 +0530

    Add files via upload

---
+++ 5/main.py
@@ -0,0 +1,11 @@
+import webapp2
+
+class HomePage(webapp2.RequestHandler):
+    def get(self):
+        i=1;
+        while(i<=10):
+            self.response.write(i*10)
+            self.response.write("<html><br></html>")
+            i=i+1;
+
+app = webapp2.WSGIApplication([("/",HomePage)],debug=True)
